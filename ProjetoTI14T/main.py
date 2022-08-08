c0cdfcdc5a70c398517584b2304a370494e2aef8
Igorpv2004 <103595439+Igorpv2004@users.noreply.github.com>
2022-08-08 16:26:18 -0300

    Add files via upload

---
+++ ProjetoTI14T/main.py
@@ -0,0 +1,6 @@
+from ControlPessoa import  ControlPessoa
+
+
+if __name__ == "__main__":
+    controle = ControlPessoa()
+    controle.operacao()#chama o método de operação de dados da classe control2
